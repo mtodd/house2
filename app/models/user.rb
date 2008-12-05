28240c001b33f5b13e8a09c5edbd9bc9042911b1
Matt Todd <chiology@gmail.com>
2008-12-05 03:23:57 -0500

    Added user model properties.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@
 class User
   include DataMapper::Resource
   
-  property :id,     Serial
-  property :login,  String
+  property :id,               Serial
+  property :login,            String
+  property :crypted_password, String
+  property :salt,             String
+  property :created_at,       DateTime
+  property :updated_at,       DateTime
   
 end
